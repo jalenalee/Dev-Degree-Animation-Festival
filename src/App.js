41e5ce5e7ddb526a9973fb3aa687e31c60dddd39
Derek Ledoux <derek.ledoux@shopify.com>
2020-04-24 08:38:42 -0400

    loading base project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { motion } from "framer-motion"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='App'>
+      <motion.div 
+        animate = {{rotate: 360, scale: 1.5}}
+        transition = {{ ease: "easeInOut", duration: 1, flip: Infinity }} />
+    </section>
+  )
 }
 
 export default App;
